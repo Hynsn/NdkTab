b5a7373f623307d831bc09a2ed723a65fbd8c45d
Hynsn <hynson@163.com>
2022-03-27 11:46:36 +0800

    move code from  SimpleGallery

---
+++ stdlib/src/main/java/com/ktcode/leet/Array.kt
@@ -0,0 +1,31 @@
+package com.hynson.algorithm
+
+import kotlin.Array
+
+class Array {
+
+    private fun findMatrix(matrix:Array<IntArray>,x:Int){
+//        matrix[0].size
+        val a = (matrix[0][0]).compareTo(x)
+        if(a == 0){
+
+        }
+        else if(a > 0) {
+
+        }
+        else {
+
+        }
+        when{
+            a == 0 -> {
+
+            }
+            a > 0 ->{
+
+            }
+            else  ->{
+
+            }
+        }
+    }
+}
