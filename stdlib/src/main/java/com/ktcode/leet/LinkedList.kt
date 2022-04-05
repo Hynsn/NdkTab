0a5680e990aa2397b2d4b9fb39b560d657822c87
Hynson <hynson@163.com>
2022-04-05 16:26:20 +0800

    kt LinkedList

--- stdlib/src/main/java/com/ktcode/leet/LinkedList.kt
+++ stdlib/src/main/java/com/ktcode/leet/LinkedList.kt
@@ -1,9 +1,19 @@
 package com.ktcode.leet
 
 fun main(){
+    val n1 = Node(1)
+    val n2 = Node(2)
+    val n3 = Node(3)
+
+    n1.next = n2
+
+    n2.next = n3
 
 }
 
 class LinkedList{
     
+}
+class Node(value: Int) {
+    var next: Node? = null
 }
